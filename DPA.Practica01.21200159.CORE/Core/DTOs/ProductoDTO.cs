9ed52ddf88b1b87be44702468e5edf7a44c3ea8f
csaravia <csaravia@csr>
2025-10-18 19:53:41 +0000

    productoDTO

---
+++ DPA.Practica01.21200159.CORE/Core/DTOs/ProductoDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA.Practica01._21200159.CORE.Core.DTOs
+{
+    public class ProductoDTO
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public int? Stock { get; set; }
+        public decimal? Price { get; set; }
+        public int? Discount { get; set; }
+        public bool? IsActive { get; set; }
+
+    }
+}
